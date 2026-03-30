4fdec512edd7619fbb9e215aaf2820e12fe31169
daniyamz <terkimbiterungwa@gmail.com>
2026-03-30 12:00:45 +0100

    Operator

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	input := processor.GetInput()
 
-	fmt.Println("Make a selection: ")
+	fmt.Println("*****Make a selection*****")
+	fmt.Println(" 1. Last word")
+	fmt.Println(" 2. Capitalize")
+	fmt.Println(" 3. Delete Index")
 	choice := processor.GetChoice()
 	processor.Process(input, choice)
 }
--- processor/operator.go
+++ processor/operator.go
@@ -13,27 +13,39 @@ func GetInput() string {
 }
 
 func GetChoice() int {
-	var choice int
-	fmt.Print("Enter Digit: ")
-	fmt.Scanln(&choice)
-	return choice
-}
-func Process(GetInput string, Getchoice int) {
-	switch Getchoice {
-	case 1:
-		fmt.Println("Last word", GetInput)
-	case 2:
-		fmt.Println("Capitalized", strings.ToUpper(GetInput))
-	case 3:
-		var index int
-		fmt.Print("Enter index: ")
-		fmt.Scanln(&index)
-		char := []rune(GetInput)
-		if index < 0 || index > len(char) {
-			fmt.Println("Error: Index out of range")
-		}
-		result := string(append(char[:index], char[index+1:]...))
-		fmt.Println(result)
-	}
+	for {
+		var choice int
+		fmt.Print("Enter Digit: ")
+		fmt.Scanln(&choice)
 
+		if choice != 1 && choice != 2 && choice != 3 {
+			fmt.Println("Wrong Selection. Try again.")
+			continue
+		}
+		return choice
+	}
+}
+
+func Process(GetInput string, Getchoice int) {
+	for {
+		switch Getchoice {
+		case 1:
+			last := string(GetInput[len(GetInput)-1])
+			fmt.Println("Last word", last)
+		case 2:
+			fmt.Println("Capitalized", strings.ToUpper(GetInput))
+		case 3:
+			var index int
+			fmt.Print("Enter index: ")
+			fmt.Scanln(&index)
+			char := []rune(GetInput)
+			if index < 0 || index >= len(char) {
+				fmt.Println("Error: Index out of range. Try again")
+				continue
+			}
+			result := string(append(char[:index], char[index+1:]...))
+			fmt.Println(result)
+		}
+
+	}
 }
